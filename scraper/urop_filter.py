cc0053da7bcb2a2d2c3feab98f5ac1ba6f69839d
Yang Yan <yangawesome@gmail.com>
2018-09-15 18:20:41 -0400

    relpath

--- scraper/urop_filter.py
+++ scraper/urop_filter.py
@@ -3,6 +3,19 @@ from bs4 import BeautifulSoup
 from dateutil.parser import parse
 import sys
 import pymysql
+import os
+
+def toRelPath(origPath):
+	"""Converts path to path relative to current script
+
+	origPath:	path to convert
+	"""
+	try:
+		if not hasattr(toRelPath, "__location__"):
+			toRelPath.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		return os.path.join(toRelPath.__location__, origPath)
+	except NameError:
+		return origPath
 
 orig_stdout = sys.stdout
 f = open(toRelPath('../website/data.js'), 'w')
